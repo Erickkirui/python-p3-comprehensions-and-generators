e62820ec28449d825e96015b715791f3dc2715bc
Erick <erickkirui653@gmail.com>
2023-05-17 19:30:43 -0400

    start

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    num_list = [ (n*2) for n in range(0,8)]
+    
+    print (num_list)
 
 def make_exclamation(sentence_list):
     pass
